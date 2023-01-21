c34929fde860ab75c4f620099918927c852e8dc6
Jonatas Rodolfo Pereira dos Santos <rodolfostark@ufrn.edu.br>
2023-01-21 20:58:37 -0300

    feat: implementing the layout component

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,36 +1,133 @@
 import { useEffect, useState } from 'react';
 import { getAllStudents } from './client';
+import { Breadcrumb, Layout, Menu, Table, Spin, Empty } from 'antd';
+import {
+  FileOutlined,
+  PieChartOutlined,
+  UserOutlined,
+  DesktopOutlined,
+  TeamOutlined,
+  LoadingOutlined
+} from '@ant-design/icons';
 
 import './App.css';
 
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+
+const columns = [
+  {
+    title: 'Id',
+    dataIndex: 'id',
+    key: 'id',
+  },
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Email',
+    dataIndex: 'email',
+    key: 'email',
+  },
+  {
+    title: 'Gender',
+    dataIndex: 'gender',
+    key: 'gender',
+  },
+];
+
+const antIcon = (
+  <LoadingOutlined
+    style={{
+      fontSize: 24,
+    }}
+    spin
+  />
+);
+
 function App() {
+  const [collapsed, setCollapsed] = useState(false);
   const [students, setStudents] = useState([]);
+  const [fetching, setFetching] = useState(true);
 
   const fetchStudents = () => {
     getAllStudents()
-    .then(response => response.json())
-    .then(data => setStudents(data));
+      .then(response => response.json())
+      .then(data => {
+        setStudents(data);
+        setFetching(false);
+      });
   }
   useEffect(() => {
     console.log("App mounted");
     fetchStudents();
   }, []);
 
-  if (students.length === 0) {
-    return <div>Loading...</div>
+  const renderStudents = () => {
+    if (fetching) {
+      return (
+        <Spin indicator={antIcon} />
+      )
+    }
+    if (students.length <= 0) {
+      return (
+        <Empty />
+      )
+    }
+    return (
+      <Table 
+        dataSource={students} 
+        columns={columns} 
+        bordered
+        title={() => 'Students'}
+        pagination={{ pageSize: 50 }}
+        scroll={{ y: 240 }}
+        rowKey={(student) => student.id}
+      />
+    )
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Students</h1>
-        <ul>
-          {students.map(student => (
-            <li key={student.id}>{student.name} - {student.email}</li>
-          ))}
-        </ul>
-      </header>
-    </div>
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider collapsible collapsed={collapsed} onCollapse={setCollapsed}>
+        <div className="logo" />
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+          <Menu.Item key="1" icon={<PieChartOutlined />}>
+            Option 1
+          </Menu.Item>
+          <Menu.Item key="2" icon={<DesktopOutlined />}>
+            Option 2
+          </Menu.Item>
+          <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+            <Menu.Item key="3">Tom</Menu.Item>
+            <Menu.Item key="4">Bill</Menu.Item>
+            <Menu.Item key="5">Alex</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+            <Menu.Item key="6">Team 1</Menu.Item>
+            <Menu.Item key="8">Team 2</Menu.Item>
+          </SubMenu>
+          <Menu.Item key="9" icon={<FileOutlined />}>
+            Files
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout className="site-layout">
+        <Header className="site-layout-background" style={{ padding: 0 }} />
+        <Content style={{ margin: '0 16px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+          <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+            { renderStudents() }
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>By Amigoscode ©2023</Footer>
+      </Layout>
+    </Layout>
   )
 }
 
